88d98ba5d714d1aa2851f1cc62025bb1fc577c39
Tatiana Sokolova <tatiana.sokolova@devexperts.com>
2018-02-26 12:55:00 +0500

    app main

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Content from './src/components/content/content.component'
+
+class App extends Component {
+	render() {
+		return (
+	    	<div className="App">
+				<Content />
+	    	</div>
+		);
+	}
+}
+
+export default App;
